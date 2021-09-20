3e0084bb31bcdc0f37b3ad66179fb267167cff88
Matthew Miller <sysnom@hotmail.com>
2021-09-20 14:29:26 -0400

    Add project files.

---
+++ Project2/ClientApp/src/app/app.module.ts
@@ -0,0 +1,42 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
+
+import { AppComponent } from './app.component';
+import { NavMenuComponent } from './nav-menu/nav-menu.component';
+import { HomeComponent } from './home/home.component';
+import { CounterComponent } from './counter/counter.component';
+import { FetchDataComponent } from './fetch-data/fetch-data.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatButtonModule } from '@angular/material/button';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavMenuComponent,
+    HomeComponent,
+    CounterComponent,
+    FetchDataComponent
+  ],
+  imports: [
+    BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
+    HttpClientModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    MatButtonModule,
+    FormsModule,
+    RouterModule.forRoot([
+      { path: '', component: HomeComponent, pathMatch: 'full' },
+    ]),
+    BrowserAnimationsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ Project2/ClientApp/src/app/home/home.component.ts
@@ -0,0 +1,36 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  styleUrls: ['./home.component.css'],
+  templateUrl: './home.component.html',
+})
+export class HomeComponent {
+  model: CarParts = new CarParts();
+  queryStr: string = '';
+  constructor() {  }
+
+  ngOnInit() {
+  }
+
+  queryList(event: any) {
+    console.log(this.queryStr);
+
+    // sanitize query
+    for (let character of this.queryStr) {
+      if (character === 'patternMatching') {
+        this.queryStr.replace(character, '');
+      }
+    }
+
+    // filter data on sanitized query.
+    this.model.parts = this.model.parts.filter(str =>
+      str.indexOf(this.queryStr) === -1);
+
+    console.log(this.model.parts);
+  }
+}
+
+export class CarParts {
+  parts = ['tire', 'wheel', 'engine'];
+}
